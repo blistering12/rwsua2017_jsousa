be02292d3cf7f2b11631871821366a32ff913550
Jorge Sousa <jorgemsousa@ua.pt>
2017-03-10 15:05:25 +0000

    ultima atualizacao

--- player_jsousa/src/player_jsousa_node.cpp
+++ player_jsousa/src/player_jsousa_node.cpp
@@ -8,19 +8,36 @@
 
 #include <iostream>
 #include <vector>
+
+//ROS INCLUDES
+#include "ros/ros.h"
+
+//Local INCLUDES
 #include <rwsua2017_libs/player.h>
 #include <rwsua2017_msgs/MakeAPlay.h>
 
 using namespace std;
+using namespace boost;
 namespace rwsua2017
 {
 	class MyPlayer: public Player
 	{
+	    ros::Subscriber sub;
+
 	    public:
 	    MyPlayer(string argin_name, string argin_teamname): Player(argin_name,argin_teamname)
 	    {
+
+		sub = n.subscribe("/make_a_play", 1000, &MyPlayer::makeAPlayCallback, this);
+
 	    	cout<<"Initialised MyPlayer"<<endl;
 	    }
+
+	   void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg)
+	   {
+		cout << "I received a MakeAPlay message" << endl;
+		cout << "max_displacement = " << msg->max_displacement << endl;
+	   }
 
 	    vector<string> teammates; // std::vector<boost::shared_ptr<Player>> teammates;
 	};
@@ -29,8 +46,9 @@ namespace rwsua2017
 
 using namespace rwsua2017;
 
-int main()
+int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "player_jsousa");
     //Creating an instance of class Player
 //    Player player("jsousa");
 //    Player player("jsousa","blue");
@@ -46,5 +64,6 @@ int main()
     cout << "teammates" << endl;
 
 
+    ros::spin();
     return 1;
 }
