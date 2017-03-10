20d0f905304bcc8f873e353368d7ba2b12fb84d3
Jorge Sousa <jorgemsousa@ua.pt>
2017-03-10 11:41:37 +0000

    aqui vai disto

--- player_jsousa/src/player_jsousa_node.cpp
+++ player_jsousa/src/player_jsousa_node.cpp
@@ -8,44 +8,12 @@
 
 #include <iostream>
 #include <vector>
+#include <rwsua2017_libs/player.h>
+#include <rwsua2017_msgs/MakeAPlay.h>
 
 using namespace std;
-namespace rwsua2017_jsousa
+namespace rwsua2017
 {
-	class Player
-	{
-	    public:
-
-	    //Constructor with the same name as the class
-	    Player(string argin_name, string argin_teamname="green") { this->name = argin_name; setTeamName(argin_teamname);}
-
-	    //Set team name, if given a correct team name (accessor)
-	    int setTeamName(string team)
-	    {
-		if (team=="red" || team=="green" || team=="blue")
-		{
-		    this->team = team;
-		    return 1;
-		}
-		else
-		{
-		    cout << "cannot set team name to " << team << endl;
-		    return 0;
-		}
-	    }
-
-	    //Gets team name (accessor)
-	    string getTeam(void) {return team;}
-
-	    string name; //A public atribute
-
-	    void setTeamName(void) {setTeamName("red");}
-
-
-	    private:
-	    string team;
-	};
-
 	class MyPlayer: public Player
 	{
 	    public:
@@ -55,19 +23,11 @@ namespace rwsua2017_jsousa
 	    }
 
 	    vector<string> teammates; // std::vector<boost::shared_ptr<Player>> teammates;
-	    
-	    void GetTeamMates()
-	    {
-		for (size_t i=0;i<this->teammates.size();i++) //size_t = unsigned long int
-		{
-			cout << this->teammates[i] << endl;
-		}
-	    }
 	};
 }
 
 
-using namespace rwsua2017_jsousa;
+using namespace rwsua2017;
 
 int main()
 {
@@ -82,9 +42,9 @@ int main()
     player.teammates.push_back("vsilva");
 
     cout << "player.name is " << player.name << endl;
-    cout << "team is " << player.getTeam() << endl;
+    cout << "team is " << player.get_team_name() << endl;
     cout << "teammates" << endl;
 
-    player.GetTeamMates();
+
     return 1;
 }
